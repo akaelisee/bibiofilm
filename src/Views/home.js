246932454284f66b04f9a148a483f41499b21333
akaelisee <akajeanelisee@gmail.com>
2021-12-28 20:50:35 +0100

    modif

--- src/Views/home.js
+++ src/Views/home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Cookie from '../Components/cookie'
+import DarkLight from '../Components/darkLight'
 
 const Home = () => {
+
     return (
         <div>
             Home
+           <p> lorem ispm </p> 
+            <DarkLight />
+            <Cookie />
         </div>
     )
 }
--- src/icons/index.js
+++ src/icons/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhotoVideo } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
 export const IconLogo = () => {
     return <FontAwesomeIcon icon={faPhotoVideo} className='logo__icon'/>
 }
+
+export const Moon = () => {
+    return (
+        <div className='fa-moom'>
+            <FontAwesomeIcon icon={faMoon}/>
+        </div>
+    )
+}
+
+export const Sun = () => {
+    return (
+        <div className='fa-sun'>
+            <FontAwesomeIcon icon={faSun}/>
+        </div>
+    )
+}
+
