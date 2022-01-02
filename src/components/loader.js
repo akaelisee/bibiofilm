cc3f00ccb6e9e48a1c0909d70966f169d22a21a2
akaelisee <akajeanelisee@gmail.com>
2022-01-03 00:27:01 +0100

    add loader

---
+++ src/components/loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return <div className="loader"> 
+        <div className='loading'> 
+            <div className='rond'> </div>
+        </div>
+    </div>
+}
+
+export default Loader
